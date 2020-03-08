6f2b5e16a7bd5b6715d766c449aef91e0ab8e361
xuhongtao <xht0@qq.com>
2020-03-08 14:46:44 +0800

    修改

--- src/main/java/com/cn/mall/tin/common/api/ResultCode.java
+++ src/main/java/com/cn/mall/tin/common/api/ResultCode.java
@@ -12,7 +12,7 @@ public enum  ResultCode implements IErrorCode {
     SUCCESS(200, "操作成功"),
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
+    UNAUTHORIZED(401, "暂未登录或token已经过期！"),
     FORBIDDEN(403, "没有相关权限");
     private long code;
     private String message;
